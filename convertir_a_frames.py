8a9d00a811a34bce2995757be87b920192f389c3
AgostoSound <agostosound@gmail.com>
2020-12-16 10:38:04 -0300

    Ya anda, era el gitignore

--- convertir_a_frames.py
+++ convertir_a_frames.py
@@ -31,31 +31,32 @@ def main():
     directorios = os.listdir(fuente)
 
     for arch in directorios:
-        filename = Path(fuente + '/' + arch)
+        filename = fuente + '/' + arch
         nom_salida = destino + '/' + arch + '.csv'
+        if '.txt' in filename:
 
-        # Carga track de Elan
-        t = TrackElan(filename)
-        df = t.data_frame  # obtiene los datos en Pandas DataFrame
-        fps = 24  # frames por segundo del video
+            # Carga track de Elan
+            t = TrackElan(filename)
+            df = t.data_frame  # obtiene los datos en Pandas DataFrame
+            fps = 24  # frames por segundo del video
 
-        # Busca el tiempo final
-        index_final = len(df) - 1
-        tiempo_final = df['end'].max()
+            # Busca el tiempo final
+            index_final = len(df) - 1
+            tiempo_final = df['end'].max()
 
-        # Arma vectores de Frames, Tiempo y Categ (vec de Nones)
-        rango = int(np.ceil(tiempo_final * fps))
-        frames = np.arange(0, rango)
-        vec_time = []
-        for samp in range(rango):
-            vec_time.append(round(samp / fps, 3))
-        categ = ['Nada'] * (rango)
+            # Arma vectores de Frames, Tiempo y Categ (vec de Nones)
+            rango = int(np.ceil(tiempo_final * fps))
+            frames = np.arange(0, rango)
+            vec_time = []
+            for samp in range(rango):
+                vec_time.append(round(samp / fps, 3))
+            categ = ['Nada'] * (rango)
 
-        arreglo_inicial = [frames, vec_time, categ]
-        new_df = pd.DataFrame(arreglo_inicial, index=['Frames', 'Time', 'Categ']).transpose()
+            arreglo_inicial = [frames, vec_time, categ]
+            new_df = pd.DataFrame(arreglo_inicial, index=['Frames', 'Time', 'Categ']).transpose()
 
-        df_ready = asigna_categ(new_df, df, fps)
-        df_ready.to_csv(nom_salida)
+            df_ready = asigna_categ(new_df, df, fps)
+            df_ready.to_csv(nom_salida)
 
 #-----------------------------------------------------------------------------
 
